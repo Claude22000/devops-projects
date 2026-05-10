3a86dfe07a7767b1ffd0517044b9f2a4f0e1c7ff
Claudio Palacio <124849280+ClaudePal20@users.noreply.github.com>
2026-05-10 12:32:08 -0700

    reading security group for github runners instead of creating it

--- github-runners-aws/github-runners-ansible-aws/terraform/modules/network/main.tf
+++ github-runners-aws/github-runners-ansible-aws/terraform/modules/network/main.tf
@@ -9,20 +9,20 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_security_group" "github_runner_sg" {
+data "aws_security_group" "github_runner_sg" {
   name        = "github-runner-sg"
   description = "Security group for GitHub Runner"
   vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "ec2_allow_all_outbound" {
-    security_group_id = aws_security_group.github_runner_sg.id
+    security_group_id = data.aws_security_group.github_runner_sg.id
     ip_protocol = "-1"
     cidr_ipv4 = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.github_runner_sg.id
+  security_group_id = data.aws_security_group.github_runner_sg.id
 
   ip_protocol = "tcp"
   from_port   = 22
