253d930c0443faa01dd94569c08227cf1d45fe35
Claudio Palacio <124849280+ClaudePal20@users.noreply.github.com>
2026-05-24 17:39:17 -0700

    updated local_hosts

--- emr-simulation/back-end/config/settings.py
+++ emr-simulation/back-end/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jiwbf+nd-0(gsg0qw$!pnlo$)$+nrten!c+jm&i^#58#8fl74o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
